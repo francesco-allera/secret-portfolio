9950e3f6bea4f7c9cf6f4245ef2065b2caa9680c
Francesco Allera <francescoallera@gmail.com>
2024-06-08 08:20:27 +0200

    Refactoring

--- dist/loading.js
+++ dist/loading.js
@@ -7,16 +7,4 @@ const loader = (animation, delay) => {
     }, animation + delay);
 };
 
-/* function loader(animation, delay) {
-    setTimeout(() => {
-        loading.style.opacity = 0;
-
-        setTimeout(() => {
-            loading.remove();
-        }, delay);
-
-    }, animation + delay);
-
-} */
-
 window.addEventListener('load', () => loader(4775, 175));
