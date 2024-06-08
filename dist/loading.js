fb69cac6a1e0242cd3dba2f34f1cafb29c53c14d
Francesco Allera <francescoallera@gmail.com>
2024-06-08 23:55:20 +0200

    Change on loader

--- dist/loading.js
+++ dist/loading.js
@@ -3,8 +3,10 @@ const loading = document.querySelector('#loading');
 const loader = (animation, delay) => {
     setTimeout(() => {
         loading.style.opacity = 0;
-        setTimeout(() => loading.remove(), delay);
+        // setTimeout(() => loading.remove(), delay);
     }, animation + delay);
 };
 
-window.addEventListener('load', () => loader(4775, 175));
+window.addEventListener('load', () => {
+    // loader(6300, 200);
+});
