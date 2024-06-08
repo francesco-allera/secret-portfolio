8765b78a046bd660be6d832f81d31dd526153bfb
Francesco Allera <francescoallera@gmail.com>
2024-06-08 08:05:52 +0200

    Refactoring loading

--- dist/loading.js
+++ dist/loading.js
@@ -1,8 +1,13 @@
-/* Variables */
 const loading = document.querySelector('#loading');
 
-/* Loading */
-function loader(animation, delay) {
+const loader = (animation, delay) => {
+    setTimeout(() => {
+        loading.style.opacity = 0;
+        setTimeout(() => loading.remove(), delay);
+    }, animation + delay);
+};
+
+/* function loader(animation, delay) {
     setTimeout(() => {
         loading.style.opacity = 0;
 
@@ -12,9 +17,6 @@ function loader(animation, delay) {
 
     }, animation + delay);
 
-}
+} */
 
-/* After loading all resources */
-window.addEventListener('load', function() {
-    loader(4775, 175);
-});
+window.addEventListener('load', () => loader(4775, 175));
