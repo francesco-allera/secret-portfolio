a2aa47d6a69c413d9d9500661d5a8fedd0f24dc7
Francesco Allera <francescoallera@gmail.com>
2024-06-09 04:19:56 +0200

    fix

--- dist/loading.js
+++ dist/loading.js
@@ -1,26 +1,18 @@
-const loading = document.querySelector('#loading');
-
 const loader = (animation, delay) => {
+    const loading = document.createElement('div');
+    loading.id = 'loading';
+    document.body.appendChild(loading);
+
+    window.removeEventListener('focus', load);
+
     setTimeout(() => {
         loading.style.opacity = 0;
         setTimeout(() => loading.remove(), delay);
     }, animation + delay);
 };
 
-document.addEventListener('DOMContentLoaded', () => {
-    let pageIsLoaded = false, pageIsFocused = document.hasFocus();
+const load = () => loader(6300, 100);
 
-    window.addEventListener('load', () => {
-        pageIsLoaded = true;
-        if (pageIsLoaded && pageIsFocused) {
-            loader(6300, 100);
-        }
-    });
-
-    window.addEventListener('focus', () => {
-        pageIsFocused = true;
-        if (pageIsLoaded && pageIsFocused) {
-            loader(6300, 100);
-        }
-    });
+window.addEventListener('load', () => {
+    window.addEventListener('focus', load);
 });
